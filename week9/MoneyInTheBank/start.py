acdeba5525ca3833a26616313478abff8131985a
deyanskiq <deyanskiq@abv.bg>
2015-06-01 22:52:27 +0300

    Money in the bank

---
+++ week9/MoneyInTheBank/start.py
@@ -0,0 +1,10 @@
+from bank_controller import BankController
+from Bank_view import BankView
+from settings import DATABASE, BLOCK_AFTER_N_FAILED_ATTEMPTS, BLOCK_FOR_N_MINUTES
+
+controller = BankController(DATABASE,
+                            block_after_n_logins=BLOCK_AFTER_N_FAILED_ATTEMPTS,
+                            block_for_n_minutes=BLOCK_FOR_N_MINUTES)
+view = BankView(controller)
+
+view.start_taking_commands()
